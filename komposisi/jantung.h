af6200688481cbe4657fbb017b52e45da2eed86b
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-05-27 20:10:09 +0700

    komposisi (class jantung)

--- komposisi/jantung.h
+++ komposisi/jantung.h
@@ -1,2 +1,22 @@
+#include <string>
+#include <iostream>
 #ifndef JANTUNG_H
 #define JANTUNG_H
+
+using namespace std;
+
+class jantung
+{
+public:
+    jantung()
+    {
+        cout << "jantung dihidupkan\n";
+    }
+
+    ~jantung()
+    {
+        cout << "jantung dimatikan\n";
+    }
+};
+
+#endif
