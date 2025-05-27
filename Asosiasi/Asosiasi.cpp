3b4b6bc8cb37f1c5cbc3a5bfcbbd9ef08ccd5f58
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-05-27 18:54:41 +0700

    class dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -17,5 +17,28 @@ class pasien{ //Asosiasi
             cout << "Pasien \"" << nama
             << "\" tidak ada\n";
         }
+            void tambahDokter(dokter*);
+            void cetakDokter();
 
+};
+
+class dokter {
+    public:
+        string nama;
+        vector<pasien*> daftar_pasien;
+
+        dokter(string pNama): nama(pNama) {
+            cout << "Doktor \"" << nama << "\" ada\n";
+        }
+        ~dokter(){
+            cout << "Dokterr \"" << nama
+            << "\" tidak ada\n";
+        }
+            void tambahPasien(pasien*);
+            void cetakPasien();
+
+};
+
+void pasien:: tambahDokter(dokter* pDokter){
+    
 }
