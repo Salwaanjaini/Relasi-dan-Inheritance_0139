35a97e127e26330b337cce40c8e572dac0876daa
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-05-27 18:31:28 +0700

    anak.ibu

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -1,1 +1,28 @@
-#ifndef
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+    public:
+        string nama;
+        vector<anak*> daftar_anak;
+
+        ibu(string pNama): nama(pNama){
+            cout << "Ibu \"" << nama << "\" ada\n";
+        }
+
+        ibu(){
+            cout << "Ibu \"" << nama << "\" tidak ada\n";
+        }
+        void tambahanAnak(anak*);
+        void cetakAnak();
+};
+void ibu::tambahkanAnak(){
+    cout << "Daftar Anak dari ibu \"" <<
+    this->nama << "\":\n";
+    for (int i = 0; i < daftar_anak.size(): i++){
+        cout << daftar_anak[i]->nama << endl;
+    }
+    cout << endl;
+
+} 
+#endif
