b548edcb3a96791b63c2a48affe31cefee917281
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-05-27 18:58:21 +0700

    class pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -40,5 +40,15 @@ class dokter {
 };
 
 void pasien:: tambahDokter(dokter* pDokter){
-    
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menenangi pasien \""
+    << this->nama << "\":\n";
+    //auto digunakan dalam perulangan
+    for (auto& a : daftar_dokter){
+        cout << a->nama <<"\n";
+    }
+    cout << endl;
 }
